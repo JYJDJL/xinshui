0c87846e2014098453c47b1381f9b24a48989f3e
yunjieli2018/liutao <129230256@qq.com>
2019-03-16 09:46:46 +0800

    Initial commit

---
+++ src/main/java/com/lt/xinshui/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.lt.xinshui;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(XinshuiApplication.class);
+    }
+
+}
---
+++ src/main/java/com/lt/xinshui/XinshuiApplication.java
@@ -0,0 +1,13 @@
+package com.lt.xinshui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XinshuiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XinshuiApplication.class, args);
+    }
+
+}
